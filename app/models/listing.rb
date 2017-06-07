2042af00cb3bea087448147997f32af775cbda63
HogDogTheGod <throweymcthrowerson@gmail.com>
2017-06-07 15:51:09 -0400

    splitting dev storage to local

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,16 @@
 class Listing < ApplicationRecord
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.jpg", 
-    validate_media_type: false,
-    :storage => :cloudinary,
-    :cloudinary_credentials => Rails.root.join("config/cloudinary.yml"),
-    :path => ':id/:style/:filename'
-  validates_attachment :image, 
-    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  if Rails.env.development?
+    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.jpg", 
+      validate_media_type: false
+    validates_attachment :image, 
+      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  else
+    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing_ls4ayk.jpg", 
+      validate_media_type: false,
+      :storage => :cloudinary,
+      :cloudinary_credentials => Rails.root.join("config/cloudinary.yml"),
+      :path => ':id/:style/:filename'
+    validates_attachment :image, 
+      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  end
 end
