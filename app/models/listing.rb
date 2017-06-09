a8313dbff01a75839f4445f29bce2060c782e604
HogDogTheGod <throweymcthrowerson@gmail.com>
2017-06-09 11:21:49 -0400

    adding validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,15 +2,19 @@ class Listing < ApplicationRecord
   if Rails.env.development?
     has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.jpg", 
       validate_media_type: false
-    validates_attachment :image, 
-      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
   else
     has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing_ls4ayk.jpg", 
       validate_media_type: false,
       :storage => :cloudinary,
       :cloudinary_credentials => Rails.root.join("config/cloudinary.yml"),
       :path => ':id/:style/:filename'
-    validates_attachment :image, 
-      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
   end
+
+  validates_attachment :image, 
+    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  validates_attachment_presence :image
+  validates :name, :description, :price, 
+    presence: true
+  validates :price, 
+    numericality: { greater_than: 0 }
 end
